8d693eef72dd66b79233de512fb7c6995db77f8b
surajtalwade8-eng <surajtalwade8@gmail.com>
2026-02-20 18:02:57 +0530

    Refactor service worker cache management

--- sw.js
+++ sw.js
@@ -1,40 +1,24 @@
-const CACHE = "cwc-tech-v5";
-const FILES = [
-  "./",
-  "./technician-app.html",
-  "./manifest.json",
-  "./icon-192.png"
-];
+var CACHE = 'cwc-v3';
+var FILES = ['/', '/technician-app.html', '/manifest.json', '/icon-192.png'];
 
-self.addEventListener("install", function(e) {
+self.addEventListener('install', function(e){
   e.waitUntil(
-    caches.open(CACHE).then(function(c) {
-      return c.addAll(FILES);
-    })
+    caches.open(CACHE).then(function(c){ return c.addAll(FILES); })
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function(e) {
+self.addEventListener('activate', function(e){
   e.waitUntil(
-    caches.keys().then(function(keys) {
-      return Promise.all(
-        keys.filter(function(k) { return k !== CACHE; })
-            .map(function(k) { return caches.delete(k); })
-      );
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){return k!==CACHE;}).map(function(k){return caches.delete(k);}));
     })
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", function(e) {
+self.addEventListener('fetch', function(e){
   e.respondWith(
-    fetch(e.request).then(function(res) {
-      var clone = res.clone();
-      caches.open(CACHE).then(function(c) { c.put(e.request, clone); });
-      return res;
-    }).catch(function() {
-      return caches.match(e.request);
-    })
+    caches.match(e.request).then(function(r){ return r || fetch(e.request); })
   );
 });
